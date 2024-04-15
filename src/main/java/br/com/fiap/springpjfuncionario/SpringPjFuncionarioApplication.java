b5dc67e0e2a7da5420eafb2ad01bfe74c2fc90ec
SudreErick <sudrerick@gmail.com>
2024-04-15 08:23:38 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/springpjfuncionario/SpringPjFuncionarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.springpjfuncionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPjFuncionarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPjFuncionarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/springpjfuncionario/SpringPjFuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.springpjfuncionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPjFuncionarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
